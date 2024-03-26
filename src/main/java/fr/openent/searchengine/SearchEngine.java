a3c10b9f1bc92535feb54f9293de4c837c6c6c6c
mariusestaque <marius.estaque@opendigitaleducation.com>
2024-03-26 11:30:20 +0100

    fix: WB-2749, add missing promise at verticle startup

--- src/main/java/fr/openent/searchengine/SearchEngine.java
+++ src/main/java/fr/openent/searchengine/SearchEngine.java
@@ -20,13 +20,14 @@
 package fr.openent.searchengine;
 
 import fr.openent.searchengine.controllers.SearchEngineController;
+import io.vertx.core.Promise;
 import org.entcore.common.http.BaseServer;
 
 public class SearchEngine extends BaseServer {
 
 	@Override
-	public void start() throws Exception {
-		super.start();
+	public void start(Promise<Void> startPromise) throws Exception {
+		super.start(startPromise);
 
 		addController(new SearchEngineController(config.getInteger("max-sec-time-allowed", 4),
 				config.getInteger("paging-size-per-collection", 10), config.getInteger("search-word-min-size", 4)));
